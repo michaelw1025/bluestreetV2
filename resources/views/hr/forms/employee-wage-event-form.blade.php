38939db2d67344f3fa9d6c5a79c4c1a6070b6ada
Mike Williamson <michaelw1025@gmail.com>
2018-04-06 02:27:01 -0500

    fixed wage event area

--- resources/views/hr/forms/employee-wage-event-form.blade.php
+++ resources/views/hr/forms/employee-wage-event-form.blade.php
@@ -1,37 +1,36 @@
 
         <h5 class="alert alert-info mt-5 toggle-section" id="employee-wage-event">Wage Event Scale</h5>
+        
         <div class="print-section form-row align-items-center employee-wage-event {{ $errors->has('progression') ? '' : 'd-none' }}">
-        
-        @if($wageProgressions)
-            <table class="table table-bordered   ">
-                <caption>Enter Appropriate Dates For Event Scale</caption>
-                <thead class="thead-light">
-                    <tr>
-                        <th scope="col"><button type="button" class="btn btn-warning clear-wage-event-scale" >Clear Scale</button></th>
-                        @foreach($wageProgressions as $wageProgression)
-                        <th scope="col" class="text-center">{{$wageProgression->month}}</th>
-                        @endforeach
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th>Event Dates</th>
-                        @foreach($wageProgressions as $wageProgression)
-                        @php $wageEventValue = false; @endphp
-                        @foreach($employee->wageProgression as $employeeProgression)
-                            @if($employeeProgression->id == $wageProgression->id)
-                                @php $wageEventValue = true; @endphp
-                                @break
-                            @endif
-                        @endforeach
-                        <td class="text-center"><input type="text" class="form-control d-none" name="wage_event[{{$loop->index}}][month]" value="{{$wageProgression->id}}"><input type="text" class="form-control ui-datepicker-prev date-pick wage-event-scale-date" name="wage_event[{{$loop->index}}][date]" value="{{ $wageEventValue == true ? $employeeProgression->pivot->date->format('m-d-Y') : '' }}"></td>
-                        @endforeach
-                    </tr>
-                </tbody>
-            </table>
-        @endif
-        <small class="text-danger">{{ $errors->first('progression') }}</small>
+        <p class="ml-1 text-secondary">Enter Appropriate Dates For Wage Event Scale</p>
+        </div> <!-- end form row -->
 
-        
+        <div class="print-section form-row align-items-center employee-wage-event {{ $errors->has('progression') ? '' : 'd-none' }}">
+
+        @if($wageProgressions)
+
+        @foreach($wageProgressions as $wageProgression)
+
+        <div class="col-xl-4 my-1">
+            <label class="sr-only" for="wage_event[{{$loop->index}}][month]">{{$wageProgression->month}}</label>
+            <div class="input-group">
+                <div class="input-group-prepend">
+                    <div class="input-group-text">{{$wageProgression->month}}</div>
+                </div>
+                <input type="text" class="form-control d-none" name="wage_event[{{$loop->index}}][month]" value="{{$wageProgression->id}}">
+                @php $wageEventValue = false; @endphp
+                    @foreach($employee->wageProgression as $employeeProgression)
+                        @if($employeeProgression->id == $wageProgression->id)
+                            @php $wageEventValue = true; @endphp
+                            @break
+                        @endif
+                    @endforeach
+                <input type="text" class="form-control ui-datepicker-prev date-pick wage-event-scale-date" name="wage_event[{{$loop->index}}][date]" value="{{ $wageEventValue == true ? $employeeProgression->pivot->date->format('m-d-Y') : '' }}">
+            </div>
+        </div>
+
+        @endforeach
+        <button type="button" class="btn btn-warning clear-wage-event-scale ml-1 mt-1" >Clear Scale</button>
+        @endif
 
         </div> <!-- end form row -->
--- resources/views/hr/forms/employee-wage-form.blade.php
+++ resources/views/hr/forms/employee-wage-form.blade.php
@@ -1,16 +1,17 @@
 
         <h5 class="alert alert-info mt-5 toggle-section" id="employee-wage">Wage Progression</h5>
+        
         <div class="print-section form-row align-items-center employee-wage {{ $errors->has('progression') ? '' : 'd-none' }}">
         
         @if($wageTitles)
             @foreach($wageTitles as $wageTitle)
-            <table class="table table-bordered progression-{{$wageTitle->description}} wage-progression-table {{isset($employee->job) ? ($employee->job[0]->wageTitle[0]->id == $wageTitle->id ? '' : 'd-none') : 'd-none'}}">
-                <caption><span class="text-info">Current Wage</span> || <span class="text-danger">Eligible Wage</span></caption>
+            <table class="table table-bordered table-sm progression-{{$wageTitle->description}} wage-progression-table {{isset($employee->job) ? ($employee->job[0]->wageTitle[0]->id == $wageTitle->id ? '' : 'd-none') : 'd-none'}}">
+                <caption><span class="text-info">Current Wage</span></caption>
                 <thead class="thead-light">
                     <tr>
                         <th scope="col"></th>
                         @foreach($wageTitle->wageProgression as $wageProgression)
-                        <th scope="col" class="text-center {{ isset($employee) ? ($employee->wage_difference == $wageProgression->month ? 'bg-danger text-white' : '') : '' }}">{{$wageProgression->month}}</th>
+                        <th scope="col" class="text-center">{{$wageProgression->month}}</th>
                         @endforeach
                     </tr>
                 </thead>
@@ -18,7 +19,7 @@
                     <tr>
                         <th><span class="text-danger prevent-print">*</span>&nbsp;{{$wageTitle->description}}</th>
                         @foreach($wageTitle->wageProgression as $wageProgression)
-                        <td class="text-center {{ isset($employee) ? ($employee->wage_progression_wage_title_count > '0' ? ($employee->wageProgressionWageTitle[0]->id == $wageProgression->pivot->id ? 'bg-info text-white' : '') : '') : (old('progression') == $wageProgression->pivot->id ? 'bg-info text-white' : '') }}">${{$wageProgression->pivot->amount}} &nbsp <input type="radio" name="progression" value="{{$wageProgression->pivot->id}}" {{ isset($employee) ? ($employee->wage_progression_wage_title_count > '0' ? ($employee->wageProgressionWageTitle[0]->id == $wageProgression->pivot->id ? 'checked' : '') : '') : (old('progression') == $wageProgression->pivot->id ? 'checked' : '') }}></td>
+                        <td class="text-center {{ isset($employee) ? ($employee->wage_progression_wage_title_count > '0' ? ($employee->wageProgressionWageTitle[0]->id == $wageProgression->pivot->id ? 'bg-info text-white' : '') : '') : (old('progression') == $wageProgression->pivot->id ? 'bg-info text-white' : '') }}">${{$wageProgression->pivot->amount}} &nbsp; <input type="radio" name="progression" value="{{$wageProgression->pivot->id}}" {{ isset($employee) ? ($employee->wage_progression_wage_title_count > '0' ? ($employee->wageProgressionWageTitle[0]->id == $wageProgression->pivot->id ? 'checked' : '') : '') : (old('progression') == $wageProgression->pivot->id ? 'checked' : '') }}></td>
                         @endforeach
                     </tr>
                 </tbody>
