ac46b4fada7f6eb907939cfdcfb9aef35bf23778
Mike Williamson <michaelw1025@gmail.com>
2018-05-07 22:39:40 -0500

    Configure for cllimlebitweb server

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="jumbotron text-center home-jumbotron">
-  <h1 class="display-4">Welcome to <span class="text-primary"><strong>BlueStreet</strong></span></h1>
+  <h1 class="display-4">Welcome to <span class="text-primary"><strong>LebWeb</strong></span></h1>
   
 </div>
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="jumbotron jumbotron-fluid home-jumbotron">
   <div class="container">
-    <a class="rounded nav-link display-4 bg-dark text-white pt-5 pb-5" href=""><span class="text-primary">BlueStreet</span>&nbsp Bidding</a>
+    <a class="rounded nav-link display-4 bg-dark text-white pt-5 pb-5" href=""><span class="text-primary">Lebanon</span>&nbsp Bidding</a>
   </div>
 </div>
 @endsection
